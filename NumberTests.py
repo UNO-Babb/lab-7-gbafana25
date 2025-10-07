3082cab916af0bb9b0481118a3f5105014931f4b
Gareth M. <gkm25@protonmail.com>
2025-10-07 11:28:51 -0500

    lab7 done

--- NumberTests.py
+++ NumberTests.py
@@ -10,8 +10,29 @@ def isThreeOrFive(n):
 
 def isPrime(p):
   """Returns boolean (True/False) if the value given is prime."""
-
+  for i in range(1, p+1):
+    if p % i == 0 and i != 1 and i != p:
+      return False
   return True
+
+def oneThousandFirstPrime():
+  prime_count = 0
+  i = 2
+  while True:
+    if isPrime(i):
+      prime_count += 1
+      if prime_count == 10_001:
+        return i
+    i+=1
+
+def palindromeProduct():
+  largest = 0
+  for i in range(100, 1000):
+    for j in range(100, 1000):
+      prod = i*j
+      if str(prod) == str(prod)[::-1] and prod > largest:
+        largest = prod
+  return largest
 
 def isEven(n):
   """Returns boolean about given value being even."""
@@ -43,15 +64,17 @@ def fibonacciSequence(value):
 
 #Test your new functions in this main
 def main():
-  knownPrimes = [3, 7, 11, 13, 17]
+  
+  #print("10001st prime: "+str(oneThousandFirstPrime()))
 
-  num = int(input("Enter a number: "))
+  print("largest palindrome product: "+str(palindromeProduct()))
+  #num = int(input("Enter a number: "))
 
-  if isPrime(num):
-    print("%d is a prime number" %(num))
+  #if isPrime(num):
+  #  print("%d is a prime number" %(num))
 
-  if isEven(num):
-    print("%d is an even number" %(num))
+  #if isEven(num):
+  #  print("%d is an even number" %(num))
 
 
 if __name__ == '__main__':
